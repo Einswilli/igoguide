e121e713fdcca2a15411a02ed9c7aa3631075e27
lord-lion <sjammel1234@gmail.com>
2022-10-12 08:25:23 +0000

    igo_correction: textes...

--- igoguide/settings.py
+++ igoguide/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'igoguide.urls'
